63984a6bb981baea7a0689f91613f600cd2216ec
fuzionu <fuzionu@gmail.com>
2023-03-22 20:35:20 +0100

    add tests for checking input case

--- test/test/test_accepted.py
+++ test/test/test_accepted.py
@@ -8,3 +8,29 @@ def test_accepted():
     program_output = run(user_input)
     # then
     assert program_output == "Welcome to my computer quiz!\nDo you want to play? Okay! Let's play :)\nWhat does CPU stand for? Correct!\nWhat does GPU stand for? Correct!\nWhat does RAM stand for? Correct!\nWhat does PSU stand for? Correct!\nYou got 4 questions correct\nYou got 100.0% correct\n"
+
+
+def test_accepted_lowercase():
+    # given
+    user_input = 'yes\nCentral processing unit\nGraphics Processing unit\nRandom access memory\nPower supply\n'
+    # when
+    program_output = run(user_input)
+    # then
+    assert program_output == "Welcome to my computer quiz!\nDo you want to play? Okay! Let's play :)\nWhat does CPU stand for? Correct!\nWhat does GPU stand for? Correct!\nWhat does RAM stand for? Correct!\nWhat does PSU stand for? Correct!\nYou got 4 questions correct\nYou got 100.0% correct\n"
+
+
+def test_accepted_uppercase():
+    # given
+    user_input = 'YES\nCentral processing unit\nGraphics Processing unit\nRandom access memory\nPower supply\n'
+    # when
+    program_output = run(user_input)
+    # then
+    assert program_output == "Welcome to my computer quiz!\nDo you want to play? Okay! Let's play :)\nWhat does CPU stand for? Correct!\nWhat does GPU stand for? Correct!\nWhat does RAM stand for? Correct!\nWhat does PSU stand for? Correct!\nYou got 4 questions correct\nYou got 100.0% correct\n"
+
+def test_accepted_mixed_case():
+    # given
+    user_input = 'yEs\nCentral processing unit\nGraphics Processing unit\nRandom access memory\nPower supply\n'
+    # when
+    program_output = run(user_input)
+    # then
+    assert program_output == "Welcome to my computer quiz!\nDo you want to play? Okay! Let's play :)\nWhat does CPU stand for? Correct!\nWhat does GPU stand for? Correct!\nWhat does RAM stand for? Correct!\nWhat does PSU stand for? Correct!\nYou got 4 questions correct\nYou got 100.0% correct\n"
