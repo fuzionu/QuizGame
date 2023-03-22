ed67337935601dcc6145251af013ba7691468e13
fuzionu <fuzionu@gmail.com>
2023-03-22 20:51:38 +0100

    add a correct way to exit program

--- main.py
+++ main.py
@@ -5,45 +5,46 @@ def main():
 
     if playing.lower() != "yes":
         print("Bye...")
-        quit()
 
-    print("Okay! Let's play :)")
-    score = 0
+    elif playing.lower() == "yes":
 
-    answer = input("What does CPU stand for? ")
+        print("Okay! Let's play :)")
+        score = 0
 
-    if answer.lower() == "central processing unit":
-        print("Correct!")
-        score += 1
-    else:
-        print("Incorrect!")
+        answer = input("What does CPU stand for? ")
 
-    answer = input("What does GPU stand for? ")
+        if answer.lower() == "central processing unit":
+            print("Correct!")
+            score += 1
+        else:
+            print("Incorrect!")
 
-    if answer.lower() == "graphics processing unit":
-        print("Correct!")
-        score += 1
-    else:
-        print("Incorrect!")
+        answer = input("What does GPU stand for? ")
 
-    answer = input("What does RAM stand for? ")
+        if answer.lower() == "graphics processing unit":
+            print("Correct!")
+            score += 1
+        else:
+            print("Incorrect!")
 
-    if answer.lower() == "random access memory":
-        print("Correct!")
-        score += 1
-    else:
-        print("Incorrect!")
+        answer = input("What does RAM stand for? ")
 
-    answer = input("What does PSU stand for? ")
+        if answer.lower() == "random access memory":
+            print("Correct!")
+            score += 1
+        else:
+            print("Incorrect!")
 
-    if answer.lower() == "power supply":
-        print("Correct!")
-        score += 1
-    else:
-        print("Incorrect!")
+        answer = input("What does PSU stand for? ")
 
-    print("You got " + str(score) + " questions correct")
-    print("You got " + str((score / 4) * 100) + "% correct")
+        if answer.lower() == "power supply":
+            print("Correct!")
+            score += 1
+        else:
+            print("Incorrect!")
+
+        print("You got " + str(score) + " questions correct")
+        print("You got " + str((score / 4) * 100) + "% correct")
 
 
 if __name__ == '__main__':
