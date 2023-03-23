d448d5f774d66dc1217a80ff25a0281741e656f3
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-03-23 18:44:46 +0100

    clean

--- test/test/test_accepted.py
+++ test/test/test_accepted.py
@@ -27,6 +27,7 @@ def test_accepted_uppercase():
     # then
     assert program_output == "Welcome to my computer quiz!\nDo you want to play? Okay! Let's play :)\nWhat does CPU stand for? Correct!\nWhat does GPU stand for? Correct!\nWhat does RAM stand for? Correct!\nWhat does PSU stand for? Correct!\nYou got 4 questions correct\nYou got 100.0% correct\n"
 
+
 def test_accepted_mixed_case():
     # given
     user_input = 'yEs\nCentral processing unit\nGraphics Processing unit\nRandom access memory\nPower supply\n'
